82c9f1606d26c9d76c1be41863df4fed13da9c78
EC2 Default User <ec2-user@ip-10-154-239-248.ec2.internal>
2016-10-12 15:55:33 +0000

    Updated crontab creation

--- cronCloudWatchUpdate.sh
+++ cronCloudWatchUpdate.sh
@@ -8,9 +8,14 @@ crontab -l > $TMP_CRON_FILE
 UPDATE_SCRIPT=update-cloudwatch-metrics.py
 RUN_DIR=`pwd`
 UPDATE_SCRIPT_PATH=$RUN_DIR/$UPDATE_SCRIPT
+
+#write out current crontab
+crontab -l | grep -v $UPDATE_SCRIPT > $TMP_CRON_FILE
+
 #echo new cron into cron file
-echo "* * * * *  cd $RUN_DIR ; python $UPDATE_SCRIPT_PATH" >> $TMP_CRON_FILE
+echo "* * * * *  cd $RUN_DIR ; python $UPDATE_SCRIPT_PATH >/dev/null" >> $TMP_CRON_FILE
 
 #install new cron file
-crontab -u $USER $TMP_CRON_FILE
+crontab $TMP_CRON_FILE
+
 rm $TMP_CRON_FILE
