1d291d9638c96ef03f95d9cc7e42b1bfbda0965a
mustard57 <ken.tune@marklogic.com>
2016-10-27 16:06:28 +0100

    Mod

--- update-cloudwatch-metrics.py
+++ update-cloudwatch-metrics.py
@@ -310,7 +310,7 @@ def check_subscription_exists(topicName,email):
 	if(len(matching_subscriptions)):
 		print email+" is subscribed to topic "+topicName+" already"
 	else:
-		print "Subscribing "+EMAIL+" to topic "+topicName
+		print "Subscribing "+email+" to topic "+topicName
 		snsConn.subscribe(topicARN,PROTOCOL,email)	
 
 check_subscription_exists(config.SERVER_NAME,config.EMAIL_FOR_SNS)
