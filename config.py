8a7e9834ee4f08f3527960bd646fc7593288a5cc
EC2 Default User <ec2-user@ip-10-63-162-148.ec2.internal>
2016-11-03 16:40:15 +0000

    Restore default SNS email config

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ PASSWORD="cloudwatch-monitoring-user"
 HOST="localhost"
 SERVER_NAME="cloudwatch-demo-rest"
 SERVER_DATABASE="cloudwatch-demo-content"
-EMAIL_FOR_SNS="ken.tune@marklogic.com"
+EMAIL_FOR_SNS="youremail@yourdomain.com"
 AWS_REGION="us-east-1"
 
